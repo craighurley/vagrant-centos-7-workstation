4e248d5bae3dc0dae11e417da29c95c27facc53b
Craig Hurley <craig@thehurley.com>
2017-11-15 22:38:37 +1300

    Fix gnome install command.

--- scripts/workstation.sh
+++ scripts/workstation.sh
@@ -5,11 +5,11 @@ set -ex
 systemctl start firewalld
 systemctl enable firewalld
 
-# yum group install -y "GNOME Desktop"
+yum group install -y "GNOME Desktop"
 
 # enable graphical display at boot (commented out by default to save resources -- use startx when a GUI is needed)
-# unlink /etc/systemd/system/default.target
-# ln -sf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
+#unlink /etc/systemd/system/default.target
+#ln -sf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
 
 yum install -y epel-release \
     vim \
